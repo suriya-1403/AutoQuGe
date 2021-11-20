f044c9c45995a418d126ff69a77eaa3f6a02f6c8
SuriyaKrishnan <suriya@outlook.in>
2021-11-20 12:47:21 +0530

    Docker initialised

--- app.py
+++ app.py
@@ -1,37 +1,104 @@
-from flask import Flask, render_template, session, redirect
-from functools import wraps
+from flask import Flask, render_template, request, url_for, redirect, session
 import pymongo
-
+import bcrypt
+#set app as a Flask instance 
 app = Flask(__name__)
-# app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+#encryption relies on secret keys so they could be run
+app.secret_key = "testing"
+#connoct to your Mongo DB database
+client = pymongo.MongoClient("mongodb://mongodb0.example.com:27017")
 
-# # Database
-# client = pymongo.MongoClient('localhost', 27017)
-# db = client.user_login_system
+#get the database name
+db = client.get_database('AutoQuGen')
+#get the particular collection that contains the data
+records = db.register
 
-
-# Decorators
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
+#assign URLs to have a particular route 
+@app.route("/", methods=['post', 'get'])
+def index():
+    message = ''
+    #if method post in index
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+    if request.method == "POST":
+        user = request.form.get("fullname")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        #if found in database showcase that it's found 
+        user_found = records.find_one({"name": user})
+        email_found = records.find_one({"email": email})
+        if user_found:
+            message = 'There already is a user by that name'
+            return render_template('signUp.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('signUp.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('signUp.html', message=message)
         else:
-            return redirect('/')
+            #hash the password and encode it
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            #assing them in a dictionary in key value pairs
+            user_input = {'name': user, 'email': email, 'password': hashed}
+            #insert it in the record collection
+            records.insert_one(user_input)
+            
+            #find the new created account and its email
+            user_data = records.find_one({"email": email})
+            new_email = user_data['email']
+            #if registered redirect to logged in as the registered user
+            return render_template('logged_in.html', email=new_email)
+    return render_template('signUp.html')
 
-    return wrap
 
 
-# Routes
-from user import routes
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        #check if email exists in database
+        email_found = records.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            #encode the password and check if it matches
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('index.html')
 
 
-@app.route('/')
-def home():
-    return render_template('signup.html')
-
-
-@app.route('/dashboard/')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
+if __name__ == "__main__":
+  app.run(debug=True)
--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask
 from app import app
 from user.models import User
 
-@app.route('/user/signup',methods=['POST'])
+
+@app.route('/user/signup', methods=['POST'])
 def signup():
     return User().signup()
+
+
+@app.route('/user/signout')
+def signout():
+    return User().signout()
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return User().login()
