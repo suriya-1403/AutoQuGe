76e71c9a12c2693f45f44b90e83491b4b910e213
SuriyaKrishnan <suriya@outlook.in>
2021-11-26 05:50:27 +0530

    staged changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 import pymongo
 import bcrypt
+from fpdf import FPDF
 #set app as a Flask instance 
 app = Flask(__name__)
 #encryption relies on secret keys so they could be run
@@ -11,8 +12,10 @@ client = pymongo.MongoClient(port=27017)
 #get the database name
 db = client.get_database('AutoQuGen')
 #get the particular collection that contains the data
-records = db.users
-
+UserRecord = db.users
+QuesRecord = db.questions
+class PDF(FPDF):
+     pass 
 #assign URLs to have a particular route 
 @app.route("/", methods=['post', 'get'])
 def index():
@@ -26,8 +29,8 @@ def index():
         password1 = request.form.get("password")
         # password2 = request.form.get("password2")
         #if found in database showcase that it's found 
-        user_found = records.find_one({"name": user})
-        email_found = records.find_one({"email": email})
+        user_found = UserRecord.find_one({"name": user})
+        email_found = UserRecord.find_one({"email": email})
         if user_found:
             message = 'There already is a user by that name'
             return render_template('index.html', message=message)
@@ -43,10 +46,10 @@ def index():
             #assing them in a dictionary in key value pairs
             user_input = {'name': user, 'email': email, 'password': hashed}
             #insert it in the record collection
-            records.insert_one(user_input)
+            UserRecord.insert_one(user_input)
             
             #find the new created account and its email
-            user_data = records.find_one({"email": email})
+            user_data = UserRecord.find_one({"email": email})
             new_email = user_data['email']
             #if registered redirect to logged in as the registered user
         return render_template('logged_in.html', email=new_email)
@@ -65,7 +68,7 @@ def login():
         password = request.form.get("password")
 
         #check if email exists in database
-        email_found = records.find_one({"email": email})
+        email_found = UserRecord.find_one({"email": email})
         if email_found:
             email_val = email_found['email']
             passwordcheck = email_found['password']
@@ -90,6 +93,33 @@ def logged_in():
         return render_template('logged_in.html', email=email)
     else:
         return redirect(url_for("login"))
+@app.route('/add',methods=['POST','GET'])
+def add():
+    if "email" in session:
+        email = session["email"]
+        if request.method == "POST":
+            question = request.form.get("question")
+            answer = request.form.get("answer")
+            mark = request.form.get("marks")
+            challenge = request.form.get("challenge")
+            #insert the question and answer in the database
+            QuesRecord.insert_one({"question": question, "answer": answer, "marks": mark, "challenge": challenge})
+            return redirect(url_for('add'))
+        return render_template('add.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/generate', methods=['POST','GET'])
+def generate():
+    if "email" in session:
+        email = session["email"]
+        if request.method == "POST":
+            pdf = PDF(orientation='P', unit='mm', format='A4')
+            pdf.add_page()
+            pdf.output('test.pdf','F')
+        return render_template('generate.html', email=email)
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
@@ -100,6 +130,3 @@ def logout():
         return render_template('index.html')
 
 
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=5000,debug=True)
